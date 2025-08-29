95fdc52f66f0cd945d20129749fa5be3d598544d
Moheeb <moheebshams@gmail.com>
2025-08-29 08:11:14 -0700

    create productDetail layout

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('header/', views.header, name='header'),
     path('hero/', views.hero, name='hero'),
